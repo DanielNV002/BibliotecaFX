cbc9339ee24ee59c228bbaf4194584a400b4fb20
Daniel <nonprivado@gmail.com>
2025-02-04 20:30:15 +0100

    Creado Proyecto no crea tablas

--- src/main/java/org/example/bibliotecajavafx/Proyect/Main.java
+++ src/main/java/org/example/bibliotecajavafx/Proyect/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) {
         crearTablas();
     }
-
     public static void crearTablas() {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         Session session = sessionFactory.openSession();
